cff9e1791ec2e68d0166091fc80ce4da6d1e79f1
nearbystays <86144003+nearbystays@users.noreply.github.com>
2021-12-14 19:58:54 -0800

    Update todo.js

--- scripts/todo.js
+++ scripts/todo.js
@@ -103,52 +103,52 @@ window.onload = function() {
     objectStore.openCursor().onsuccess = function(event) {
       let cursor = event.target.result;
         // if there is still another cursor to go, keep runing this code
-        if(cursor) {
-          // create a list item to put each data item inside when displaying it
-          const listItem = document.createElement('li');
+      if(cursor) {
+        // create a list item to put each data item inside when displaying it
+        const listItem = document.createElement('li');
 
-          // check which suffix the deadline day of the month needs
-          if(cursor.value.day == 1 || cursor.value.day == 21 || cursor.value.day == 31) {
-            daySuffix = "st";
-          } else if(cursor.value.day == 2 || cursor.value.day == 22) {
-            daySuffix = "nd";
-          } else if(cursor.value.day == 3 || cursor.value.day == 23) {
-            daySuffix = "rd";
-          } else {
-            daySuffix = "th";
-          }
-
-          // build the to-do list entry and put it into the list item via innerHTML.
-          listItem.innerHTML = cursor.value.taskTitle + ' — ' + cursor.value.hours + ':' + cursor.value.minutes + ', ' + cursor.value.month + ' ' + cursor.value.day + daySuffix + ' ' + cursor.value.year + '.';
-
-          if(cursor.value.notified == "yes") {
-            listItem.style.textDecoration = "line-through";
-            listItem.style.color = "rgba(255,0,0,0.5)";
-          }
-
-          // put the item item inside the task list
-          taskList.appendChild(listItem);
-
-          // create a delete button inside each list item, giving it an event handler so that it runs the deleteButton()
-          // function when clicked
-          const deleteButton = document.createElement('button');
-          listItem.appendChild(deleteButton);
-          deleteButton.innerHTML = 'X';
-          // here we are setting a data attribute on our delete button to say what task we want deleted if it is clicked!
-          deleteButton.setAttribute('data-task', cursor.value.taskTitle);
-          deleteButton.onclick = function(event) {
-            deleteItem(event);
-          }
-
-          // continue on to the next item in the cursor
-          cursor.continue();
-
-        // if there are no more cursor items to iterate through, say so, and exit the function
+        // check which suffix the deadline day of the month needs
+        if(cursor.value.day == 1 || cursor.value.day == 21 || cursor.value.day == 31) {
+          daySuffix = "st";
+        } else if(cursor.value.day == 2 || cursor.value.day == 22) {
+          daySuffix = "nd";
+        } else if(cursor.value.day == 3 || cursor.value.day == 23) {
+          daySuffix = "rd";
         } else {
-          note.innerHTML += '<li>Entries all displayed.</li>';
+          daySuffix = "th";
         }
+
+        // build the to-do list entry and put it into the list item via innerHTML.
+        listItem.innerHTML = cursor.value.taskTitle + ' — ' + cursor.value.hours + ':' + cursor.value.minutes + ', ' + cursor.value.month + ' ' + cursor.value.day + daySuffix + ' ' + cursor.value.year + '.';
+
+        if(cursor.value.notified == "yes") {
+          listItem.style.textDecoration = "line-through";
+          listItem.style.color = "rgba(255,0,0,0.5)";
+        }
+
+        // put the item item inside the task list
+        taskList.appendChild(listItem);
+
+        // create a delete button inside each list item, giving it an event handler so that it runs the deleteButton()
+        // function when clicked
+        const deleteButton = document.createElement('button');
+        listItem.appendChild(deleteButton);
+        deleteButton.innerHTML = 'X';
+        // here we are setting a data attribute on our delete button to say what task we want deleted if it is clicked!
+        deleteButton.setAttribute('data-task', cursor.value.taskTitle);
+        deleteButton.onclick = function(event) {
+          deleteItem(event);
+        }
+
+        // continue on to the next item in the cursor
+        cursor.continue();
+
+      // if there are no more cursor items to iterate through, say so, and exit the function
+      } else {
+        note.innerHTML += '<li>Entries all displayed.</li>';
       }
     }
+  }
 
   // give the form submit button an event listener so that when the form is submitted the addData() function is run
   taskForm.addEventListener('submit',addData,false);
@@ -414,3 +414,4 @@ window.onload = function() {
   // using a setInterval to run the checkDeadlines() function every second
   setInterval(checkDeadlines, 1000);
 }
+
